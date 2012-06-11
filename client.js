5c2a8c6dffdc662bfbf6cbfeef454626a06c16a3
Michael Benin <beninmichael@gmail.com>
2012-06-10 23:15:49 -0400

    js safari

--- client.js
+++ client.js
@@ -1,1 +1,51 @@
+(function()
+{
+	var wsUri = "ws://66.108.74.131/";
+	var protocol = "echo-protocol";
+
+   	websocket = new WebSocket(wsUri, protocol);
+    	websocket.addEventListener('open', function(evt)
+    	{
+        	var obj = {"msg":"A new user has joined"};
+		websocket.send(JSON.stringify(obj));
+    	});
+
+   	websocket.addEventListener('close', function (evt)
+    	{
+        	writeToScreen("Disconnected");
+			console.log(evt);
+    	});
+   
+	websocket.addEventListener('message', function(evt)
+    	{  
+			var msg = JSON.parse(evt.data);
+        	writeToScreen('<span style="color:blue;">Response: '+ msg.msg + '</span>');
+    	});
+    	
+	websocket.addEventListener('error', function(evt)
+    	{
+        	writeToScreen('<span style="color:red;">Error: </span> '+ evt.data);
+    	});
+
+	document.addEventListener('keypress', function keyPress(e) 
+	{
+		key = e ? e.which : window.event.keyCode;
+		if(key==13 && document.getElementById('chat').value !== "") 
+		{
+		        var obj = {"msg":document.getElementById("chat").value};
+			websocket.send(JSON.stringify(obj));
+			document.getElementById('chat').value = "";
+			document.getElementById('chat').focus();
+		}
+	});
+
+	function writeToScreen(message)
+	{	
+    		var pre = document.createElement("p");
+    		pre.style.wordWrap = "break-word";
+    		pre.innerHTML = message;
+    		document.getElementById('conversation').appendChild(pre);
+	};
+
+})();
 
